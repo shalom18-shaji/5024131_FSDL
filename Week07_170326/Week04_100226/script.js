0cc72e10e8517a88a0b6e74ca3d3114c31021ec0
shalom18-shaji <shalomsk18@gmail.com>
2026-04-07 03:25:35 +0000

    perfect

--- Week07_170326/Week04_100226/script.js
+++
@@ -1,8 +0,0 @@
-function showMessage() {
-  alert("Welcome to Formula 1!");
-}
-
-function showCars() {
-  document.getElementById("carStats").innerHTML =
-    "RB19: 350+ km/h | SF-23: 340+ km/h | W14: 335+ km/h";
-}
